90ba643f46fa5ee7cec5be079a6c8367bb18cd2f
Luiz Felipe <lfelipelopesti@gmail.com>
2025-10-30 14:42:18 -0300

    feat: create model, view, controller and route to list posts

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+
+Route::get('/login', function() {
+
+    return view('auth.login');
+
+})->name('login');
+
+Route::get('/register', function() {
+
+    return view('auth.register');
+
+})->name('register');
+
+
+
+
+Route::prefix('/posts')->group(function(){
+
+    Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
+});
